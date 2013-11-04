100177a9ef5b2806c8c7faf9df82025e5e5b9db7
matt.hughson <matt.hughson@6b875ac5-22ef-5333-9aac-8e7b49ed9c3a>
2013-11-04 03:11:52 +0000

    Reskinning of Mode select which will have lots of new functionality soon.
    Also includes WIP of Muti-Stage Button behaviour.

---
+++ BumpSetSpikeContentDefs/Behaviour/ButtonMultiStageDefinition.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MBHEngineContentDefs;
+using Microsoft.Xna.Framework.Content;
+
+namespace BumpSetSpikeContentDefs
+{
+    public class ButtonMultiStageDefinition : ButtonDefinition
+    {
+        public Int32 mNumStages;
+    }
+}
