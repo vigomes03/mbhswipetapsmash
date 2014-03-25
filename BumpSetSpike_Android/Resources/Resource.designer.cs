caa6c60fec9290bd77cf0ad2d1e4c8f7595689ab
matt.hughson <matt.hughson@6b875ac5-22ef-5333-9aac-8e7b49ed9c3a>
2014-03-25 04:55:33 +0000

    Updated splash screen that doesn't stretch when the orientation changes.

--- BumpSetSpike_Android/Resources/Resource.designer.cs
+++ BumpSetSpike_Android/Resources/Resource.designer.cs
@@ -95,41 +95,41 @@ namespace BumpSetSpike
 		public partial class Color
 		{
 			
-			// aapt resource value: 0x7f040009
-			public const int common_action_bar_splitter = 2130968585;
+			// aapt resource value: 0x7f030009
+			public const int common_action_bar_splitter = 2130903049;
 			
-			// aapt resource value: 0x7f040000
-			public const int common_signin_btn_dark_text_default = 2130968576;
+			// aapt resource value: 0x7f030000
+			public const int common_signin_btn_dark_text_default = 2130903040;
 			
-			// aapt resource value: 0x7f040002
-			public const int common_signin_btn_dark_text_disabled = 2130968578;
+			// aapt resource value: 0x7f030002
+			public const int common_signin_btn_dark_text_disabled = 2130903042;
 			
-			// aapt resource value: 0x7f040003
-			public const int common_signin_btn_dark_text_focused = 2130968579;
+			// aapt resource value: 0x7f030003
+			public const int common_signin_btn_dark_text_focused = 2130903043;
 			
-			// aapt resource value: 0x7f040001
-			public const int common_signin_btn_dark_text_pressed = 2130968577;
+			// aapt resource value: 0x7f030001
+			public const int common_signin_btn_dark_text_pressed = 2130903041;
 			
-			// aapt resource value: 0x7f040008
-			public const int common_signin_btn_default_background = 2130968584;
+			// aapt resource value: 0x7f030008
+			public const int common_signin_btn_default_background = 2130903048;
 			
-			// aapt resource value: 0x7f040004
-			public const int common_signin_btn_light_text_default = 2130968580;
+			// aapt resource value: 0x7f030004
+			public const int common_signin_btn_light_text_default = 2130903044;
 			
-			// aapt resource value: 0x7f040006
-			public const int common_signin_btn_light_text_disabled = 2130968582;
+			// aapt resource value: 0x7f030006
+			public const int common_signin_btn_light_text_disabled = 2130903046;
 			
-			// aapt resource value: 0x7f040007
-			public const int common_signin_btn_light_text_focused = 2130968583;
+			// aapt resource value: 0x7f030007
+			public const int common_signin_btn_light_text_focused = 2130903047;
 			
-			// aapt resource value: 0x7f040005
-			public const int common_signin_btn_light_text_pressed = 2130968581;
+			// aapt resource value: 0x7f030005
+			public const int common_signin_btn_light_text_pressed = 2130903045;
 			
-			// aapt resource value: 0x7f04000a
-			public const int common_signin_btn_text_dark = 2130968586;
+			// aapt resource value: 0x7f03000a
+			public const int common_signin_btn_text_dark = 2130903050;
 			
-			// aapt resource value: 0x7f04000b
-			public const int common_signin_btn_text_light = 2130968587;
+			// aapt resource value: 0x7f03000b
+			public const int common_signin_btn_text_light = 2130903051;
 			
 			static Color()
 			{
@@ -237,6 +237,9 @@ namespace BumpSetSpike
 			// aapt resource value: 0x7f02001e
 			public const int Splash = 2130837534;
 			
+			// aapt resource value: 0x7f02001f
+			public const int splash_centered = 2130837535;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -250,41 +253,20 @@ namespace BumpSetSpike
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050004
-			public const int hybrid = 2131034116;
+			// aapt resource value: 0x7f040004
+			public const int hybrid = 2130968580;
 			
-			// aapt resource value: 0x7f050006
-			public const int logout_button = 2131034118;
+			// aapt resource value: 0x7f040000
+			public const int none = 2130968576;
 			
-			// aapt resource value: 0x7f050000
-			public const int none = 2131034112;
+			// aapt resource value: 0x7f040001
+			public const int normal = 2130968577;
 			
-			// aapt resource value: 0x7f050001
-			public const int normal = 2131034113;
+			// aapt resource value: 0x7f040002
+			public const int satellite = 2130968578;
 			
-			// aapt resource value: 0x7f050007
-			public const int plus_one_button = 2131034119;
-			
-			// aapt resource value: 0x7f05000b
-			public const int profile_about = 2131034123;
-			
-			// aapt resource value: 0x7f050008
-			public const int profile_image = 2131034120;
-			
-			// aapt resource value: 0x7f050009
-			public const int profile_name = 2131034121;
-			
-			// aapt resource value: 0x7f05000a
-			public const int profile_nickname = 2131034122;
-			
-			// aapt resource value: 0x7f050002
-			public const int satellite = 2131034114;
-			
-			// aapt resource value: 0x7f050005
-			public const int sign_in_button = 2131034117;
-			
-			// aapt resource value: 0x7f050003
-			public const int terrain = 2131034115;
+			// aapt resource value: 0x7f040003
+			public const int terrain = 2130968579;
 			
 			static Id()
 			{
@@ -299,8 +281,8 @@ namespace BumpSetSpike
 		public partial class Integer
 		{
 			
-			// aapt resource value: 0x7f070000
-			public const int google_play_services_version = 2131165184;
+			// aapt resource value: 0x7f060000
+			public const int google_play_services_version = 2131099648;
 			
 			static Integer()
 			{
@@ -312,144 +294,128 @@ namespace BumpSetSpike
 			}
 		}
 		
-		public partial class Layout
-		{
-			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
-			
-			static Layout()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Layout()
-			{
-			}
-		}
-		
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f060021
-			public const int achievement_bend_the_rules = 2131099681;
+			// aapt resource value: 0x7f050021
+			public const int achievement_bend_the_rules = 2131034145;
 			
-			// aapt resource value: 0x7f06001e
-			public const int achievement_double_trouble = 2131099678;
+			// aapt resource value: 0x7f05001e
+			public const int achievement_double_trouble = 2131034142;
 			
-			// aapt resource value: 0x7f06001f
-			public const int achievement_fortune_500 = 2131099679;
+			// aapt resource value: 0x7f05001f
+			public const int achievement_fortune_500 = 2131034143;
 			
-			// aapt resource value: 0x7f060022
-			public const int achievement_hang_time = 2131099682;
+			// aapt resource value: 0x7f050022
+			public const int achievement_hang_time = 2131034146;
 			
-			// aapt resource value: 0x7f060023
-			public const int achievement_is_that_even_possible = 2131099683;
+			// aapt resource value: 0x7f050023
+			public const int achievement_is_that_even_possible = 2131034147;
 			
-			// aapt resource value: 0x7f060025
-			public const int achievement_lucky_20 = 2131099685;
+			// aapt resource value: 0x7f050025
+			public const int achievement_lucky_20 = 2131034149;
 			
-			// aapt resource value: 0x7f060020
-			public const int achievement_lucky_7 = 2131099680;
+			// aapt resource value: 0x7f050020
+			public const int achievement_lucky_7 = 2131034144;
 			
-			// aapt resource value: 0x7f06001d
-			public const int achievement_participation = 2131099677;
+			// aapt resource value: 0x7f05001d
+			public const int achievement_participation = 2131034141;
 			
-			// aapt resource value: 0x7f060024
-			public const int achievement_putting_the_i_in_team = 2131099684;
+			// aapt resource value: 0x7f050024
+			public const int achievement_putting_the_i_in_team = 2131034148;
 			
-			// aapt resource value: 0x7f06001c
-			public const int achievement_the_ubi = 2131099676;
+			// aapt resource value: 0x7f05001c
+			public const int achievement_the_ubi = 2131034140;
 			
-			// aapt resource value: 0x7f06001b
-			public const int app_id = 2131099675;
+			// aapt resource value: 0x7f05001b
+			public const int app_id = 2131034139;
 			
-			// aapt resource value: 0x7f060015
-			public const int auth_client_needs_enabling_title = 2131099669;
+			// aapt resource value: 0x7f050015
+			public const int auth_client_needs_enabling_title = 2131034133;
 			
-			// aapt resource value: 0x7f060016
-			public const int auth_client_needs_installation_title = 2131099670;
+			// aapt resource value: 0x7f050016
+			public const int auth_client_needs_installation_title = 2131034134;
 			
-			// aapt resource value: 0x7f060017
-			public const int auth_client_needs_update_title = 2131099671;
+			// aapt resource value: 0x7f050017
+			public const int auth_client_needs_update_title = 2131034135;
 			
-			// aapt resource value: 0x7f060018
-			public const int auth_client_play_services_err_notification_msg = 2131099672;
+			// aapt resource value: 0x7f050018
+			public const int auth_client_play_services_err_notification_msg = 2131034136;
 			
-			// aapt resource value: 0x7f060019
-			public const int auth_client_requested_by_msg = 2131099673;
+			// aapt resource value: 0x7f050019
+			public const int auth_client_requested_by_msg = 2131034137;
 			
-			// aapt resource value: 0x7f060014
-			public const int auth_client_using_bad_version_title = 2131099668;
+			// aapt resource value: 0x7f050014
+			public const int auth_client_using_bad_version_title = 2131034132;
 			
-			// aapt resource value: 0x7f060006
-			public const int common_google_play_services_enable_button = 2131099654;
+			// aapt resource value: 0x7f050006
+			public const int common_google_play_services_enable_button = 2131034118;
 			
-			// aapt resource value: 0x7f060005
-			public const int common_google_play_services_enable_text = 2131099653;
+			// aapt resource value: 0x7f050005
+			public const int common_google_play_services_enable_text = 2131034117;
 			
-			// aapt resource value: 0x7f060004
-			public const int common_google_play_services_enable_title = 2131099652;
+			// aapt resource value: 0x7f050004
+			public const int common_google_play_services_enable_title = 2131034116;
 			
-			// aapt resource value: 0x7f060003
-			public const int common_google_play_services_install_button = 2131099651;
+			// aapt resource value: 0x7f050003
+			public const int common_google_play_services_install_button = 2131034115;
 			
-			// aapt resource value: 0x7f060001
-			public const int common_google_play_services_install_text_phone = 2131099649;
+			// aapt resource value: 0x7f050001
+			public const int common_google_play_services_install_text_phone = 2131034113;
 			
-			// aapt resource value: 0x7f060002
-			public const int common_google_play_services_install_text_tablet = 2131099650;
+			// aapt resource value: 0x7f050002
+			public const int common_google_play_services_install_text_tablet = 2131034114;
 			
-			// aapt resource value: 0x7f060000
-			public const int common_google_play_services_install_title = 2131099648;
+			// aapt resource value: 0x7f050000
+			public const int common_google_play_services_install_title = 2131034112;
 			
-			// aapt resource value: 0x7f06000c
-			public const int common_google_play_services_invalid_account_text = 2131099660;
+			// aapt resource value: 0x7f05000c
+			public const int common_google_play_services_invalid_account_text = 2131034124;
 			
-			// aapt resource value: 0x7f06000b
-			public const int common_google_play_services_invalid_account_title = 2131099659;
+			// aapt resource value: 0x7f05000b
+			public const int common_google_play_services_invalid_account_title = 2131034123;
 			
-			// aapt resource value: 0x7f06000a
-			public const int common_google_play_services_network_error_text = 2131099658;
+			// aapt resource value: 0x7f05000a
+			public const int common_google_play_services_network_error_text = 2131034122;
 			
-			// aapt resource value: 0x7f060009
-			public const int common_google_play_services_network_error_title = 2131099657;
+			// aapt resource value: 0x7f050009
+			public const int common_google_play_services_network_error_title = 2131034121;
 			
-			// aapt resource value: 0x7f06000d
-			public const int common_google_play_services_unknown_issue = 2131099661;
+			// aapt resource value: 0x7f05000d
+			public const int common_google_play_services_unknown_issue = 2131034125;
 			
-			// aapt resource value: 0x7f060010
-			public const int common_google_play_services_unsupported_date_text = 2131099664;
+			// aapt resource value: 0x7f050010
+			public const int common_google_play_services_unsupported_date_text = 2131034128;
 			
-			// aapt resource value: 0x7f06000f
-			public const int common_google_play_services_unsupported_text = 2131099663;
+			// aapt resource value: 0x7f05000f
+			public const int common_google_play_services_unsupported_text = 2131034127;
 			
-			// aapt resource value: 0x7f06000e
-			public const int common_google_play_services_unsupported_title = 2131099662;
+			// aapt resource value: 0x7f05000e
+			public const int common_google_play_services_unsupported_title = 2131034126;
 			
-			// aapt resource value: 0x7f060011
-			public const int common_google_play_services_update_button = 2131099665;
+			// aapt resource value: 0x7f050011
+			public const int common_google_play_services_update_button = 2131034129;
 			
-			// aapt resource value: 0x7f060008
-			public const int common_google_play_services_update_text = 2131099656;
+			// aapt resource value: 0x7f050008
+			public const int common_google_play_services_update_text = 2131034120;
 			
-			// aapt resource value: 0x7f060007
-			public const int common_google_play_services_update_title = 2131099655;
+			// aapt resource value: 0x7f050007
+			public const int common_google_play_services_update_title = 2131034119;
 			
-			// aapt resource value: 0x7f060012
-			public const int common_signin_button_text = 2131099666;
+			// aapt resource value: 0x7f050012
+			public const int common_signin_button_text = 2131034130;
 			
-			// aapt resource value: 0x7f060013
-			public const int common_signin_button_text_long = 2131099667;
+			// aapt resource value: 0x7f050013
+			public const int common_signin_button_text_long = 2131034131;
 			
-			// aapt resource value: 0x7f060026
-			public const int leaderboard_endurnace = 2131099686;
+			// aapt resource value: 0x7f050026
+			public const int leaderboard_endurnace = 2131034150;
 			
-			// aapt resource value: 0x7f060027
-			public const int leaderboard_trick_attack = 2131099687;
+			// aapt resource value: 0x7f050027
+			public const int leaderboard_trick_attack = 2131034151;
 			
-			// aapt resource value: 0x7f06001a
-			public const int library_name = 2131099674;
+			// aapt resource value: 0x7f05001a
+			public const int library_name = 2131034138;
 			
 			static String()
 			{
@@ -464,8 +430,8 @@ namespace BumpSetSpike
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f080000
-			public const int Theme_Splash = 2131230720;
+			// aapt resource value: 0x7f070000
+			public const int Theme_Splash = 2131165184;
 			
 			static Style()
 			{
